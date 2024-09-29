05d3c8e0e6695ce9be900e32a58e5a6adf4a26b6
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2024-09-29 13:56:57 +0200

    added video background

--- src/components/WeatherDisplay/WeatherDisplay.tsx
+++ src/components/WeatherDisplay/WeatherDisplay.tsx
@@ -1,21 +1,47 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { WeatherDisplayProps } from '../../interfaces/WeatherDisplayProps';
 import './WeatherDisplay.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot, faDroplet, faEye, faWind, faTachometerAlt, faCloud, faCompass } from '@fortawesome/free-solid-svg-icons';
+const dayBackground = '/videos/daySky.mp4'; // Direktna putanja
+const nightBackground = '/videos/nightSky.mp4';
+
 
 const WeatherDisplay: React.FC<WeatherDisplayProps> = ({ weatherData, city }) => {
-    const isDayTime = () => {
-        const currentTime = Math.floor(Date.now() / 1000);
-        return currentTime >= weatherData.sunrise && currentTime < weatherData.sunset
-    }
+    const [isDay, setIsDay] = useState(false);
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+
+    useEffect(() => {
+        const isDayTime = () => {
+            const currentTime = Math.floor(Date.now() / 1000);
+            return currentTime >= weatherData.sunrise && currentTime < weatherData.sunset;
+        }
+        setIsDay(isDayTime());
+
+        if (videoRef.current) {
+            videoRef.current.src = isDay ? dayBackground : nightBackground;
+            videoRef.current.load(); 
+
+            const handleCanPlayThrough = () => {
+                videoRef.current?.play(); 
+                videoRef.current?.removeEventListener('canplaythrough', handleCanPlayThrough); 
+            };
+
+            videoRef.current.addEventListener('canplaythrough', handleCanPlayThrough);
+        }
+    }, [weatherData, isDay]);
+
     return (
-        <div className={`weather-display ${isDayTime() ? 'day' : 'night'}`}>
+        <div className={`weather-display ${isDay ? 'day' : 'night'}`}>
+            <video ref={videoRef} autoPlay muted loop className={`background-video ${isDay ? 'day' : 'night'}`}>
+                <source src={isDay ? dayBackground : nightBackground} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
             <div className='main-info-container'>
                 <div className='location-container'>
                     <div className="location-info">
                         <h2 className="city-name">{city}</h2>
-                        <FontAwesomeIcon icon={faLocationDot} style={{ color: "#ffffff" }} className="location-icon" />
+                        <FontAwesomeIcon icon={faLocationDot} style={isDay ? {color: 'rgb(5, 5, 84)'}: { color: "#ffffff" }} className="location-icon" />
                     </div>
                     <p className='weather-description'>{weatherData.description}</p>
                 </div>
@@ -28,42 +54,42 @@ const WeatherDisplay: React.FC<WeatherDisplayProps> = ({ weatherData, city }) =>
                 </div>
             </div>
             <div className='weather-info-grid'>
-                <div className={`weather-info-item ${isDayTime() ? 'day' : 'night'}`}>
+                <div className={`weather-info-item ${isDay ? 'day' : 'night'}`}>
                     <div className='info-caption'>
                         <FontAwesomeIcon icon={faDroplet} className='weather-info-icon' />
                         <p>Humidity</p>
                     </div>
                     <p>{weatherData.humidity}%</p>
                 </div>
-                <div className={`weather-info-item ${isDayTime() ? 'day' : 'night'}`}>
+                <div className={`weather-info-item ${isDay ? 'day' : 'night'}`}>
                     <div className='info-caption'>
                         <FontAwesomeIcon icon={faEye} className='weather-info-icon' />
                         <p>Visibility</p>
                     </div>
                     <p>{weatherData.visibility / 1000} km</p>
                 </div>
-                <div className={`weather-info-item ${isDayTime() ? 'day' : 'night'}`}>
+                <div className={`weather-info-item ${isDay ? 'day' : 'night'}`}>
                     <div className='info-caption'>
                         <FontAwesomeIcon icon={faWind} className='weather-info-icon' />
                         <p>Wind speed</p>
                     </div>
                     <p>{Math.round(weatherData.windSpeed)} km/h</p>
                 </div>
-                <div className={`weather-info-item ${isDayTime() ? 'day' : 'night'}`}>
+                <div className={`weather-info-item ${isDay ? 'day' : 'night'}`}>
                     <div className='info-caption'>
                         <FontAwesomeIcon icon={faTachometerAlt} className='weather-info-icon' />
                         <p>Pressure</p>
                     </div>
                     <p>{weatherData.pressure} hPa</p>
                 </div>
-                <div className={`weather-info-item ${isDayTime() ? 'day' : 'night'}`}>
+                <div className={`weather-info-item ${isDay ? 'day' : 'night'}`}>
                     <div className='info-caption'>
                         <FontAwesomeIcon icon={faCloud} className='weather-info-icon' />
                         <p>Cloudiness</p>
                     </div>
                     <p>{weatherData.cloudiness}%</p>
                 </div>
-                <div className={`weather-info-item ${isDayTime() ? 'day' : 'night'}`}>
+                <div className={`weather-info-item ${isDay ? 'day' : 'night'}`}>
                     <div className='info-caption'>
                         <FontAwesomeIcon icon={faCompass} className='weather-info-icon' />
                         <p>Wind direction</p>
