19f7d81623e789ba0941f831c8fb93cae111570a
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2024-10-01 01:01:35 +0200

    Added loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,19 @@ import WeatherDisplay from './components/WeatherDisplay/WeatherDisplay';
 import Navbar from './components/Navbar/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { MoonLoader } from 'react-spinners';
 
 function App() {
   const [city, setCity] = useState<string>('Sarajevo');
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
   const [error, setError] = useState<string | null>(null)
+  const [isLoading, setLoading] = useState<boolean>(false)
 
   useEffect(() => {
     const fetchWeather = async () => {
       if (city) {
         try {
+          setLoading(true)
           setError(null)
           const data = await fetchWeatherData(city);
           setWeatherData(data);
@@ -24,34 +27,42 @@ function App() {
           const errorMessage = error instanceof Error ? error.message : 'ERROR: An unknown error occurred';
           setWeatherData(null);
           setError(errorMessage);
+        } finally {
+          setLoading(false);
         }
       }
     }
 
     fetchWeather();
-  }, [city])
+  }, [city]);
 
   return (
     <div className="app-container">
       <Navbar>
         <Search onSearch={setCity} />
       </Navbar>
-      {!error ?
-        (
-          weatherData ?
-            <WeatherDisplay weatherData={weatherData} city={city} /> :
-            <div className='no-results'>
-              <h1>No results for "{city}"</h1>
-            </div>
-        ) :
-        <div className='error-display'>
-          <div className='error-title'>
-            <FontAwesomeIcon className='error-icon' icon={faTriangleExclamation} />
+      {isLoading && (
+        <div className="loader">
+          <MoonLoader />
+          <p>Loading...</p>
+        </div>
+      )}
+      {!isLoading && !error && (
+        weatherData ?
+          <WeatherDisplay weatherData={weatherData} city={city} /> :
+          <div className="result-info">
+            <h1>No results for "{city}"</h1>
+          </div>
+      )}
+      {!isLoading && error && (
+        <div className="error-display">
+          <div className="error-title">
+            <FontAwesomeIcon className="error-icon" icon={faTriangleExclamation} />
             <h1>ERROR</h1>
           </div>
           <p>{error}</p>
         </div>
-      }
+      )}
     </div>
   );
 
